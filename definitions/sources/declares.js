ce2d1907a2ce5821d1ab9762e737d315fa7d39ef
Max Ostapenko <bvz2001@gmail.com>
2024-08-27 11:56:16 +0000

    assert fix

--- definitions/sources/declares.js
+++ definitions/sources/declares.js
@@ -10,13 +10,15 @@ declare({
     name: "device_summary",
 });
 assert(
-    "device_summary not empty"
+    "device_summary_not_empty"
 ).query(
     ctx => `
     SELECT 
-        COUNT(0)
+        'No data for the specified date' AS error_message
     FROM ${ctx.ref("chrome-ux-report", "materialized", "device_summary")}
     WHERE yyyymm = 202407
+    GROUP BY yyyymm
+    HAVING COUNT(1) = 0
 `);
 
 declare({
@@ -25,11 +27,13 @@ declare({
     name: "country_summary",
 });
 assert(
-    "country_summary not empty"
+    "country_summary_not_empty"
 ).query(
     ctx => `
     SELECT 
-        COUNT(0)
+        'No data for the specified date' AS error_message
     FROM ${ctx.ref("chrome-ux-report", "materialized", "country_summary")}
     WHERE yyyymm = 202407
+    GROUP BY yyyymm
+    HAVING COUNT(1) = 0
 `);
