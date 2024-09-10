536adda9740754a9a6a0b129bcecd2fd15fc7b58
Max Ostapenko <max@httparchive.org>
2024-09-10 22:04:38 +0000

    optimise test tables creation

--- definitions/extra/test_env.js
+++ definitions/extra/test_env.js
@@ -1,30 +1,24 @@
 const date = constants.fn_past_month(constants.current_month);
 
-operate("test_tables", {
-  hasOutput: true,
-  disabled: !constants.is_dev_env // enabled when workflow variable env_name = "dev"
-}).queries(ctx => `
-CREATE SCHEMA IF NOT EXISTS all_dev;
+var resources_list = [
+  {datasetId: "all",              tableId: "pages"},
+  {datasetId: "all",              tableId: "requests"},
+  //{datasetId: "all",              tableId: "parsed_css"},
+  //{datasetId: "core_web_vitals",  tableId: "technologies"},
+];
 
-CREATE TABLE IF NOT EXISTS ${ctx.resolve("all", "pages")} AS
-SELECT *
-FROM httparchive.all.pages ${constants.dev_TABLESAMPLE}
-WHERE date = '${date}';
+resources_list.forEach(resource => {
+  operate(`test_table ${resource.datasetId}_${resource.tableId}`, {
+    disabled: !constants.is_dev_env // enabled when workflow variable env_name = "dev"
+  }).queries(ctx => `
+CREATE SCHEMA IF NOT EXISTS ${resource.datasetId}_dev;
 
-CREATE TABLE IF NOT EXISTS ${ctx.resolve("all", "requests")} AS
+CREATE TABLE ${resource.datasetId}_dev.dev_${resource.tableId}
+LIKE httparchive.${resource.datasetId}.${resource.tableId};
+
+INSERT INTO ${resource.datasetId}_dev.dev_${resource.tableId}
 SELECT *
-FROM httparchive.all.requests ${constants.dev_TABLESAMPLE}
+FROM httparchive.${resource.datasetId}.${resource.tableId} ${constants.dev_TABLESAMPLE}
 WHERE date = '${date}'
-
-/*
-CREATE TABLE IF NOT EXISTS ${ctx.resolve("all", "parsed_css")} AS
-SELECT *
-FROM httparchive.all.parsed_css ${constants.dev_TABLESAMPLE}
-WHERE date = '${date}';
-
-CREATE TABLE IF NOT EXISTS ${ctx.resolve("core_web_vitals", "technologies")} AS
-SELECT *
-FROM httparchive.core_web_vitals.technologies
-WHERE date = '${date}'
-*/
-`);
+  `);
+})
