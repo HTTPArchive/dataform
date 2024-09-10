1b88a731f179d96403623431fe4ba460af91fa56
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-09-10 15:24:40 +0200

    alter query post update

--- definitions/output/all/pages_stable.js
+++ definitions/output/all/pages_stable.js
@@ -1,15 +1,13 @@
-operate(`all_pages_stable alter`).tags(
+operate(`all_pages_stable_alter_pre`).tags(
   ["all_pages_stable"]
 ).queries(ctx => `
 ALTER TABLE ${ctx.ref("all", "pages")}
-ADD COLUMN IF NOT EXISTS custom_metrics_struct STRUCT<performance STRING, other STRING> OPTIONS(description="Custom metrics from WebPageTest")
+ADD COLUMN IF NOT EXISTS custom_metrics_struct STRUCT<javascript STRING, media STRING, performance STRING, other STRING> OPTIONS(description="Custom metrics from WebPageTest")
 `);
 
-let month = '2024-08-01',
-  month_YYYYMM = constants.fn_past_month(month).replace('-', '').substring(0, 6);
-
+let month = '2024-08-01';
 while (month >= '2022-07-01') {
-  operate(`all_pages_stable ${month}`, {
+  operate(`all_pages_stable_update ${month}`, {
     hasOutput: true
   }).tags(
     ["all_pages_stable"]
@@ -61,9 +59,20 @@ LANGUAGE js AS """
 UPDATE ${ctx.ref("all", "pages")}
 SET summary = PRUNE_OBJECT(summary, ["metadata", "pageid", "createDate", "startedDateTime", "archive", "label", "crawlid", "url", "urlhash", "urlShort", "wptid", "wptrun", "rank", "PageSpeed", "_adult_site", "avg_dom_depth", "doctype", "document_height", "document_width", "localstorage_size", "sessionstorage_size", "meta_viewport", "num_iframes", "num_scripts", "num_scripts_sync", "num_scripts_async", "usertiming"]),
     custom_metrics_struct = GET_CUSTOM_METRICS(custom_metrics)
-WHERE date = '${month}'
+WHERE date = '${month}';
   `)
 
   month = constants.fn_past_month(month)
   month_YYYYMM = constants.fn_past_month(month).replace('-', '').substring(0, 6)
 }
+
+
+operate(`all_pages_stable_alter_post`).tags(
+  ["all_pages_stable"]
+).queries(ctx => `
+ALTER TABLE ${ctx.ref("all", "pages")}
+DROP COLUMN IF EXISTS custom_metrics;
+
+ALTER TABLE ${ctx.ref("all", "pages")}
+RENAME COLUMN custom_metrics_struct TO custom_metrics
+`);
