af5860f2b753732af977daa85260cdd64a6e9905
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-09-11 23:23:55 +0200

    Sample data tables (#7)
    * sampled data
    
    * 10k
    
    * no partition filter required
    
    * add today constant

---
+++ definitions/output/sample_data/parsed_css_10k.js
@@ -0,0 +1,16 @@
+publish("parsed_css_10k", {
+    type: "table",
+    schema: "sample_data",
+    bigquery: {
+        partitionBy: "date",
+        clusterBy: ["client", "is_root_page", "rank", "page"]
+    },
+    tags: ["crawl_results_all"]
+}).preOps(ctx => `
+DROP TABLE ${ctx.self()};
+`).query(ctx => `
+SELECT *
+FROM ${ctx.ref("all", "parsed_css")}
+WHERE date = "${constants.current_month}" AND
+    rank <= 10000
+`);
