d7a379fcd1c9f981e4d6a421fc97bdf7b14b690d
Max Ostapenko <max@httparchive.org>
2024-09-19 20:35:53 +0000

    no reference

--- definitions/extra/test_env.js
+++ definitions/extra/test_env.js
@@ -6,19 +6,19 @@ operate("test_env", {
 }).queries(ctx => `
 CREATE SCHEMA IF NOT EXISTS all_dev;
 
-CREATE TABLE IF NOT EXISTS ${ctx.ref("all", "pages")} AS
+CREATE TABLE IF NOT EXISTS ${ctx.resolve("all", "pages")} AS
 SELECT *
 FROM httparchive.all.pages
 WHERE
   date = '${constants.current_month}'
   ${constants.dev_rank5000_filter};
 
-CREATE TABLE IF NOT EXISTS ${ctx.ref("all", "requests")} AS
+CREATE TABLE IF NOT EXISTS ${ctx.resolve("all", "requests")} AS
 SELECT *
 FROM httparchive.all.requests ${constants.dev_TABLESAMPLE}
 WHERE date = '${constants.current_month}';
 
-CREATE TABLE IF NOT EXISTS ${ctx.ref("all", "parsed_css")} AS
+CREATE TABLE IF NOT EXISTS ${ctx.resolve("all", "parsed_css")} AS
 SELECT *
 FROM httparchive.all.parsed_css
 WHERE date = '${constants.current_month}'
@@ -26,19 +26,19 @@ WHERE date = '${constants.current_month}'
 
 CREATE SCHEMA IF NOT EXISTS core_web_vitals_dev;
 
-CREATE TABLE IF NOT EXISTS ${ctx.ref("core_web_vitals", "technologies")} AS
+CREATE TABLE IF NOT EXISTS ${ctx.resolve("core_web_vitals", "technologies")} AS
 SELECT *
 FROM httparchive.core_web_vitals.technologies ${constants.dev_TABLESAMPLE}
 WHERE date = '${past_month}';
 
 CREATE SCHEMA IF NOT EXISTS blink_features_dev;
 
-CREATE TABLE IF NOT EXISTS ${ctx.ref("blink_features", "usage")} AS
+CREATE TABLE IF NOT EXISTS ${ctx.resolve("blink_features", "usage")} AS
 SELECT *
 FROM httparchive.blink_features.usage ${constants.dev_TABLESAMPLE}
 WHERE yyyymmdd = '${past_month}';
 
-CREATE TABLE IF NOT EXISTS ${ctx.ref("blink_features", "features")} AS
+CREATE TABLE IF NOT EXISTS ${ctx.resolve("blink_features", "features")} AS
 SELECT *
 FROM httparchive.blink_features.features ${constants.dev_TABLESAMPLE}
 WHERE yyyymmdd = DATE '${past_month}';
