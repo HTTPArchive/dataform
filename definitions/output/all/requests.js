0dbb96cb9d17874deacd65a12b06e91687ee2533
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-09-19 20:15:02 +0200

    backfill draft

--- definitions/output/all/requests.js
+++ definitions/output/all/requests.js
@@ -51,3 +51,87 @@ SELECT * EXCEPT (rank)
 FROM ${ctx.ref("crawl_staging", "requests")} ${constants.dev_TABLESAMPLE}
 WHERE date = '${constants.current_month}' AND client = 'mobile' AND is_root_page = FALSE AND (type != 'script' OR type IS NULL)
 `)
+
+let monthRange = [];
+for (
+  let month = '2016-01-01';
+  month < '2022-07-01';
+  month = constants.fn_past_month(month)) {
+    monthRange.push(month)
+}
+
+monthRange.forEach((month, i) => {
+  operate(`requests_backfill_from_response_bodies ${month}`).tags([
+    "response_bodies_deprecated"
+  ]).queries(ctx => `
+DELETE FROM ${ctx.resolve("all", "requests")}
+WHERE date = '${month}';
+
+INSERT INTO ${ctx.resolve("all", "requests")}
+SELECT
+  '${month}' AS date,
+  COALESCE(response_bodies._TABLE_SUFFIX, requests._TABLE_SUFFIX) AS client,
+  COALESCE(response_bodies.page, requests.page) AS page,
+  TRUE AS is_root_page,
+  COALESCE(response_bodies.page, requests.page) AS root_page,
+  COALESCE(response_bodies.url, requests.url) AS url,
+  IF(
+    AND(
+      SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$._request_type') AS STRING) = "Document",
+      MIN(SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$._index') AS INT64)) OVER (PARTITION BY page) = SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$._index') AS INT64)
+    ),
+    TRUE,
+    FALSE
+  ) AS is_main_document,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$._index') AS INT64) AS index,
+  requests.payload AS payload,
+  JSON_OBJECT(
+    "time", SAFE_CAST(JSON_VALUE(requests.payload, '$.time') AS INTEGER),
+    "method", JSON_VALUE(requests.payload, '$._method'),
+    "redirectUrl",
+    "reqHttpVersion", JSON_VALUE(requests.payload, '$.request.httpVersion'),
+    "reqHeadersSize", JSON_VALUE(requests.payload, '$.request.headersSize'),
+    "reqBodySize", JSON_VALUE(requests.payload, '$.request.bodySize'),
+    "reqCookieLen",
+    "status", JSON_VALUE(requests.payload, '$.response.status'),
+    "respHttpVersion", JSON_VALUE(requests.payload, '$.response.httpVersion'),
+    "respHeadersSize", JSON_VALUE(requests.payload, '$.response.headersSize'),
+    "respBodySize", JSON_VALUE(requests.payload, '$.response.bodySize'),
+    "respSize", JSON_VALUE(requests.payload, '$.response.content.size'),
+    "respCookieLen",
+    "expAge",
+    "mimeType", JSON_VALUE(requests.payload, '$.response.content.mimeType'),
+    "_cdn_provider", JSON_VALUE(requests.payload, '$._cdn_provider'),
+    "_gzip_save", JSON_VALUE(requests.payload, '$._gzip_save'),
+    "ext",
+    "format",
+  ) AS summary,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$.request.headers') AS JSON) AS request_headers,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$.response.headers') AS JSON) AS response_headers,
+  response_bodies.response_body AS response_body
+FROM ${ctx.resolve("response_bodies", `${constants.fn_date_underscored(month)}_*`)} AS response_bodies
+FULL OUTER JOIN ${ctx.resolve("requests", `${constants.fn_date_underscored(month)}_*`)} AS requests
+USING (page, url);
+  `)
+})
+
+
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.time') AS INTEGER) AS time,
+  JSON_EXTRACT_SCALAR(summary, '$.method') AS method,
+  JSON_EXTRACT_SCALAR(summary, '$.redirectUrl') AS redirectUrl,
+  JSON_EXTRACT_SCALAR(summary, '$.reqHttpVersion') AS reqHttpVersion,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.reqHeadersSize') AS INTEGER) AS reqHeadersSize,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.reqBodySize') AS INTEGER) AS reqBodySize,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.reqCookieLen') AS INTEGER) AS reqCookieLen,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.status') AS INTEGER) AS status,
+  JSON_EXTRACT_SCALAR(summary, '$.respHttpVersion') AS respHttpVersion,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.respHeadersSize') AS INTEGER) AS respHeadersSize,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.respBodySize') AS INTEGER) AS respBodySize,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.respSize') AS INTEGER) AS respSize,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.respCookieLen') AS INTEGER) AS respCookieLen,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$.expAge') AS INTEGER) AS expAge,
+  JSON_EXTRACT_SCALAR(summary, '$.mimeType') AS mimeType,
+  JSON_EXTRACT_SCALAR(summary, '$._cdn_provider') AS _cdn_provider,
+  SAFE_CAST(JSON_EXTRACT_SCALAR(summary, '$._gzip_save') AS INTEGER) AS _gzip_save,
+  JSON_EXTRACT_SCALAR(summary, '$.ext') AS ext,
+  JSON_EXTRACT_SCALAR(summary, '$.format') AS format,
