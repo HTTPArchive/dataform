2a7e489f35c8a4a45ee90df4b7893831980af015
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-09-19 21:31:29 +0200

    blink features report

---
+++ definitions/output/blink_features/features.js
@@ -0,0 +1,69 @@
+
+publish("features", {
+  schema: "blink_features",
+  type: "incremental",
+  protected: true,
+  bigquery: {
+    partitionBy: "yyyymmdd",
+    clusterBy: ["client", "rank"]
+  },
+  tags: ["blink_features_report"]
+}).preOps(ctx => `
+DELETE FROM ${ctx.self()}
+WHERE date = '${constants.current_month}';
+`).query(ctx => `
+CREATE TEMP FUNCTION features(payload STRING)
+RETURNS ARRAY<STRUCT<id STRING, name STRING, type STRING>> LANGUAGE js AS
+"""
+function getFeatureNames(featureMap, featureType) {
+  try {
+    return Object.entries(featureMap).map(([key, value]) => {
+      // After Feb 2020 keys are feature IDs.
+      if (value.name) {
+        return {'name': value.name, 'type': featureType, 'id': key};
+      }
+      // Prior to Feb 2020 keys fell back to IDs if the name was unknown.
+      if (idPattern.test(key)) {
+        return {'name': '', 'type': featureType, 'id': key.match(idPattern)[1]};
+      }
+      // Prior to Feb 2020 keys were names by default.
+      return {'name': key, 'type': featureType, 'id': ''};
+    });
+  } catch (e) {
+    return [];
+  }
+}
+
+var $ = JSON.parse(payload);
+if (!$._blinkFeatureFirstUsed) return [];
+
+var idPattern = new RegExp('^Feature_(\\d+)$');
+return getFeatureNames($._blinkFeatureFirstUsed.Features, 'default')
+  .concat(getFeatureNames($._blinkFeatureFirstUsed.CSSFeatures, 'css'))
+  .concat(getFeatureNames($._blinkFeatureFirstUsed.AnimatedCSSFeatures, 'animated-css'));
+""";
+
+SELECT
+  date AS yyyymmdd,
+  client,
+  url,
+  feature.feature AS feature,
+  feature.type,
+  feature.id,
+  rank
+FROM (
+  SELECT
+    date,
+    client,
+    page AS url,
+    payload,
+    rank,
+    feature
+  FROM
+    ${ctx.resolve("all", "pages")},
+    UNNEST(features) AS feature
+  WHERE
+    date = '${constants.current_month}' AND
+    is_root_page = TRUE
+)
+`)
---
+++ definitions/output/blink_features/usage.js
@@ -0,0 +1,53 @@
+publish("usage", {
+  schema: "blink_features",
+  type: "incremental",
+  protected: true,
+  tags: ["blink_features_report"]
+}).preOps(ctx => `
+DELETE FROM ${ctx.self()}
+WHERE date = '${constants.current_month}';
+`).query(ctx => `
+SELECT
+  REGEXP_REPLACE(CAST(date AS STRING), '-', '') AS yyyymmdd,
+  client,
+  id,
+  feature,
+  type,
+  num_urls,
+  total_urls,
+  num_urls / total_urls AS pct_urls,
+  sample_urls
+FROM (
+  SELECT
+    yyyymmdd AS date,
+    client,
+    id,
+    feature,
+    type,
+    COUNT(DISTINCT url) AS num_urls,
+    ARRAY_AGG(url ORDER BY rank, url LIMIT 100) AS sample_urls
+  FROM ${ctx.resolve("blink_features", "features")}
+  WHERE
+    yyyymmdd = '${constants.current_month}'
+  GROUP BY
+    yyyymmdd,
+    client,
+    id,
+    feature,
+    type
+)
+JOIN (
+  SELECT
+    date,
+    client,
+    COUNT(DISTINCT page) AS total_urls
+  FROM ${ctx.resolve("all", "pages")}
+  WHERE
+    date = '${constants.current_month}' AND
+    is_root_page = TRUE
+  GROUP BY
+    date,
+    client
+)
+USING (date, client)
+`)
