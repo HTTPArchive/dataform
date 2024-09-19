e9e09cec67b7f6b0787ae54658d90e0914f33f64
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-09-19 15:24:54 +0200

    drop if exists

--- definitions/output/sample_data/parsed_css_10k.js
+++ definitions/output/sample_data/parsed_css_10k.js
@@ -1,13 +1,13 @@
 publish("parsed_css_10k", {
-    type: "table",
-    schema: "sample_data",
-    bigquery: {
-        partitionBy: "date",
-        clusterBy: ["client", "is_root_page", "rank", "page"]
-    },
-    tags: ["crawl_results_all"]
+  type: "table",
+  schema: "sample_data",
+  bigquery: {
+    partitionBy: "date",
+    clusterBy: ["client", "is_root_page", "rank", "page"]
+  },
+  tags: ["crawl_results_all"]
 }).preOps(ctx => `
-DROP TABLE ${ctx.self()};
+DROP TABLE IF EXISTS ${ctx.self()};
 `).query(ctx => `
 SELECT *
 FROM ${ctx.ref("all", "parsed_css")}
