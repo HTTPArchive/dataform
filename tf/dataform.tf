cffddee4d9c3f0764f18b9dd355f7ce6154c2a30
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-10-12 18:01:51 +0200

    local dataform sa

--- tf/dataform.tf
+++ tf/dataform.tf
@@ -1,3 +1,7 @@
+locals {
+  dataform_service_account_email = "service-226352634162@gcp-sa-dataform.iam.gserviceaccount.com"
+}
+
 import {
   provider = google-beta
   id = "projects/${local.project}/locations/${local.region}/repositories/crawl-data"
@@ -35,4 +39,29 @@ resource "google_dataform_repository_release_config" "production" {
   region        = local.region
   repository    = "crawl-data"
   time_zone     = "Etc/UTC"
+}
+
+
+# BigQuery IAM roles for Dataform
+locals {
+  datasets = [
+    "all",
+    "core_web_vitals",
+    "blink_features",
+    "sample_data"
+  ]
+}
+
+resource "google_bigquery_dataset_iam_member" "data_editor_role" {
+  for_each = toset(local.datasets)
+
+  dataset_id = each.value
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${local.dataform_service_account_email}"
+}
+
+resource "google_project_iam_member" "bigquery_user" {
+  project = local.project
+  role    = "roles/bigquery.user"
+  member  = "serviceAccount:${local.dataform_service_account_email}"
 }
--- tf/locals.tf
+++ tf/locals.tf
@@ -3,6 +3,4 @@ locals {
   project_number        = "226352634162"
   region                = "us-central1"
   location              = "us"
-  service_account_email = "service-226352634162@gcp-sa-dataform.iam.gserviceaccount.com"
-
 }
