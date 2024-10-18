fc0e77581db0d7aaf5a6b204f3f952ed80a005d6
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-10-18 19:02:18 +0200

    format

--- definitions/sources/chrome-ux-report.js
+++ definitions/sources/chrome-ux-report.js
@@ -1,8 +1,10 @@
+const database = 'chrome-ux-report'
+
 const pastMonth = constants.fnPastMonth(constants.currentMonth)
 const pastMonthYYYYMM = pastMonth.substring(0, 7).replace('-', '')
 
 declare({
-  database: 'chrome-ux-report',
+  database,
   schema: 'materialized',
   name: 'country_summary'
 })
@@ -10,14 +12,14 @@ declare({
 assert('country_summary_not_empty').query(ctx => `
 SELECT
   'No data for the specified date' AS error_message
-FROM ${ctx.ref('chrome-ux-report', 'materialized', 'country_summary')}
+FROM ${ctx.ref(database, 'materialized', 'country_summary')}
 WHERE yyyymm = ${pastMonthYYYYMM}
 GROUP BY yyyymm
 HAVING COUNT(0) < 20000000
 `)
 
 declare({
-  database: 'chrome-ux-report',
+  database,
   schema: 'materialized',
   name: 'device_summary'
 })
@@ -25,14 +27,14 @@ declare({
 assert('device_summary_not_empty').query(ctx => `
 SELECT
   'No data for the specified date' AS error_message
-FROM ${ctx.ref('chrome-ux-report', 'materialized', 'device_summary')}
+FROM ${ctx.ref(database, 'materialized', 'device_summary')}
 WHERE date = '${pastMonth}'
 GROUP BY date
 HAVING COUNT(0) < 20000000
 `)
 
 declare({
-  database: 'chrome-ux-report',
+  database,
   schema: 'experimental',
   name: 'global'
 })
