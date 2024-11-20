425067788bad403b3fe01f30d213fe1eef0aaa8c
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-11-20 01:06:05 +0100

    tech report tables

---
+++ definitions/output/tech_reports/lighthouse.js
@@ -0,0 +1,71 @@
+const pastMonth = constants.fnPastMonth(constants.currentMonth)
+
+publish('lighthouse', {
+  schema: 'tech_reports',
+  type: 'incremental',
+  protected: true,
+  bigquery: {
+    partitionBy: 'date',
+    clusterBy: ['rank', 'geo']
+  },
+  tags: ['cwv_tech_report']
+}).query(ctx => `
+CREATE TEMPORARY FUNCTION GET_LIGHTHOUSE(
+  records ARRAY<STRUCT<
+    client STRING,
+    median_lighthouse_score_accessibility NUMERIC,
+    median_lighthouse_score_best_practices NUMERIC,
+    median_lighthouse_score_performance NUMERIC,
+    median_lighthouse_score_pwa NUMERIC,
+    median_lighthouse_score_seo NUMERIC
+>>
+) RETURNS ARRAY<STRUCT<
+name STRING,
+desktop STRUCT<
+  median_score NUMERIC
+>,
+mobile STRUCT<
+  median_score NUMERIC
+>
+>> LANGUAGE js AS '''
+const METRIC_MAP = {{
+  accessibility: 'median_lighthouse_score_accessibility',
+  best_practices: 'median_lighthouse_score_best_practices',
+  performance: 'median_lighthouse_score_performance',
+  pwa: 'median_lighthouse_score_pwa',
+  seo: 'median_lighthouse_score_seo',
+}};
+
+// Initialize the Lighthouse map.
+const lighthouse = Object.fromEntries(Object.keys(METRIC_MAP).map(metricName => {{
+  return [metricName, {{name: metricName}}];
+}}));
+
+// Populate each client record.
+records.forEach(record => {{
+  Object.entries(METRIC_MAP).forEach(([metricName, median_score]) => {{
+      lighthouse[metricName][record.client] = {{median_score: record[median_score]}};
+  }});
+}});
+
+return Object.values(lighthouse);
+''';
+
+SELECT
+  STRING(DATE(date)) as date,
+  app AS technology,
+  rank,
+  geo,
+  GET_LIGHTHOUSE(ARRAY_AGG(STRUCT(
+    client,
+    median_lighthouse_score_accessibility,
+    median_lighthouse_score_best_practices,
+    median_lighthouse_score_performance,
+    median_lighthouse_score_pwa,
+    median_lighthouse_score_seo
+
+  ))) AS lighthouse
+FROM ${ctx.ref('core_web_vitals', 'technologies')}
+WHERE date = '${pastMonth}'
+GROUP BY date, app, rank, geo
+`)
---
+++ definitions/output/tech_reports/page_weight.js
@@ -0,0 +1,60 @@
+const pastMonth = constants.fnPastMonth(constants.currentMonth)
+
+publish('page_weight', {
+  schema: 'tech_reports',
+  type: 'incremental',
+  protected: true,
+  bigquery: {
+    partitionBy: 'date',
+    clusterBy: ['rank', 'geo']
+  },
+  tags: ['cwv_tech_report']
+}).query(ctx => `
+CREATE TEMPORARY FUNCTION GET_PAGE_WEIGHT(
+  records ARRAY<STRUCT<
+    client STRING,
+    total INT64,
+    js INT64,
+    images INT64
+  >>
+) RETURNS ARRAY<STRUCT<
+  name STRING,
+  mobile STRUCT<
+    median_bytes INT64
+  >,
+  desktop STRUCT<
+    median_bytes INT64
+  >
+>> LANGUAGE js AS '''
+const METRICS = ['total', 'js', 'images'];
+
+// Initialize the page weight map.
+const pageWeight = Object.fromEntries(METRICS.map(metricName => {{
+return [metricName, {{name: metricName}}];
+}}));
+
+// Populate each client record.
+records.forEach(record => {{
+  METRICS.forEach(metricName => {{
+    pageWeight[metricName][record.client] = {{median_bytes: record[metricName]}};
+  }});
+}});
+
+return Object.values(pageWeight);
+''';
+
+SELECT
+  STRING(DATE(date)) as date,
+  app AS technology,
+  rank,
+  geo,
+  GET_PAGE_WEIGHT(ARRAY_AGG(STRUCT(
+    client,
+    median_bytes_total,
+    median_bytes_js,
+    median_bytes_image
+  ))) AS pageWeight
+FROM ${ctx.ref('core_web_vitals', 'technologies')}
+WHERE date = '${pastMonth}'
+GROUP BY date, app, rank, geo
+`)
