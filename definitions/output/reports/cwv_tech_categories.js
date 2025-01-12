d6fea3a4c4e91924a24348d8be211f6236165799
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-01-12 19:02:33 +0100

    by client

--- definitions/output/reports/cwv_tech_categories.js
+++ definitions/output/reports/cwv_tech_categories.js
@@ -8,42 +8,49 @@ publish('cwv_tech_categories', {
 /* {"dataform_trigger": "report_cwv_tech_complete", "name": "categories", "type": "dict"} */
 WITH pages AS (
   SELECT
-    root_page,
+    client,
+    root_page AS origin,
     technologies
   FROM ${ctx.ref('crawl', 'pages')}
   WHERE
-    date = '${pastMonth}' AND
-    client = 'mobile'
+    date = '${pastMonth}'
     ${constants.devRankFilter}
 ), categories AS (
   SELECT
+    client,
     category,
-    COUNT(DISTINCT root_page) AS origins
+    COUNT(DISTINCT origin) AS origins
   FROM pages,
-    UNNEST(technologies) AS t,
-    UNNEST(t.categories) AS category
-  GROUP BY category
+    UNNEST(technologies) AS tech,
+    UNNEST(tech.categories) AS category
+  GROUP BY
+    client,
+    category
 ), technologies AS (
   SELECT
+    client,
     category,
     technology,
-    COUNT(DISTINCT root_page) AS origins
+    COUNT(DISTINCT origin) AS origins
   FROM pages,
-    UNNEST(technologies) AS t,
-    UNNEST(t.categories) AS category
+    UNNEST(technologies) AS tech,
+    UNNEST(tech.categories) AS category
   GROUP BY
+    client,
     category,
     technology
 )
 
 SELECT
+  client,
   category,
   categories.origins,
   ARRAY_AGG(technology IGNORE NULLS ORDER BY technologies.origins DESC) AS technologies
 FROM categories
-JOIN technologies
+INNER JOIN technologies
 USING (category)
 GROUP BY
+  client,
   category,
   categories.origins
 ORDER BY categories.origins DESC
