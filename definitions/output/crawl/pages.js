42bf6fa4b22374b404bca8e056828bbd55e352b8
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-01-20 20:53:32 +0100

    crawl pipeline patch

--- definitions/output/crawl/pages.js
+++ definitions/output/crawl/pages.js
@@ -52,23 +52,108 @@ publish('pages', {
 DELETE FROM ${ctx.self()}
 WHERE date = '${constants.currentMonth}' AND
   client = 'desktop';
-`).query(ctx => `
-SELECT
-  *
-FROM ${ctx.ref('crawl_staging', 'pages')}
-WHERE date = '${constants.currentMonth}' AND
-  client = 'desktop'
-  ${constants.devRankFilter}
-`).postOps(ctx => `
-DELETE FROM ${ctx.self()}
-WHERE date = '${constants.currentMonth}' AND
-  client = 'mobile';
 
 INSERT INTO ${ctx.self()}
 SELECT
   *
 FROM ${ctx.ref('crawl_staging', 'pages')}
 WHERE date = '${constants.currentMonth}' AND
+  client = 'desktop'
+  ${constants.devRankFilter};
+
+DELETE FROM ${ctx.self()}
+WHERE date = '${constants.currentMonth}' AND
+  client = 'mobile';
+`).query(ctx => `
+SELECT
+  *
+FROM ${ctx.ref('crawl_staging', 'pages')}
+WHERE date = '${constants.currentMonth}' AND
   client = 'mobile'
   ${constants.devRankFilter}
+`).postOps(ctx => `
+CREATE TEMP TABLE technologies_cleaned AS (
+  WITH wappalyzer AS (
+    SELECT
+      name AS technology,
+      categories AS valid_categories
+    FROM ${ctx.ref('wappalyzer', 'technologies')}
+  ), pages AS (
+    SELECT
+      client,
+      page,
+      tech.technology,
+      tech.categories,
+      tech.info
+    FROM ${ctx.ref('crawl', 'pages')} AS pages
+    LEFT JOIN pages.technologies AS tech
+    WHERE date = '${constants.currentMonth}' ${constants.devRankFilter}
+  ), -- Identify impacted pages
+  impacted_pages AS (
+    SELECT DISTINCT
+      client,
+      page
+    FROM pages
+    WHERE
+      -- Categories are empty OR technology is corrupted OR contains invalid categories
+      (technology IS NOT NULL AND ARRAY_LENGTH(categories) = 0)
+      OR technology NOT IN (SELECT technology FROM wappalyzer)
+      OR EXISTS (
+        SELECT category
+        FROM UNNEST(categories) AS category
+        WHERE category NOT IN (SELECT DISTINCT UNNEST(valid_categories) FROM wappalyzer)
+      )
+  ), -- Flatten technologies for reconstruction
+  flattened_technologies AS (
+    SELECT
+      client,
+      page,
+      technology,
+      ARRAY(
+        SELECT category
+        FROM UNNEST(categories) AS category
+        WHERE category IN (
+          SELECT DISTINCT UNNEST(valid_categories)
+          FROM wappalyzer
+          WHERE technology = flattened_technologies.technology
+        )
+      ) AS valid_categories,
+      info
+    FROM pages
+    WHERE page IN (SELECT DISTINCT page FROM impacted_pages)
+  ), -- Reconstruct valid technologies
+  reconstructed_technologies AS (
+    SELECT
+      client,
+      page,
+      ARRAY_AGG(STRUCT(
+        f.technology,
+        IF(ARRAY_LENGTH(f.valid_categories) = 0, w.valid_categories, f.valid_categories) AS categories,
+        f.info
+      )) AS technologies
+    FROM flattened_technologies f
+    LEFT JOIN wappalyzer w
+    ON f.technology = w.technology
+    WHERE f.technology IN (SELECT technology FROM wappalyzer)
+    GROUP BY
+      client,
+      page
+  )
+
+  SELECT
+    client,
+    page,
+    r.technologies
+  FROM impacted_pages
+  LEFT JOIN reconstructed_technologies r
+  USING (client, page)
+);
+
+-- Update the crawl.pages table with the cleaned and restored technologies
+UPDATE crawl.pages
+SET technologies = technologies_cleaned.technologies
+FROM technologies_cleaned
+WHERE pages.date = '${constants.currentMonth}' AND
+  pages.client = technologies_cleaned.client AND
+  pages.page = technologies_cleaned.page;
 `)
