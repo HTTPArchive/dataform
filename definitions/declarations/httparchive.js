b64479e5dd13231abdeedd455e6d045d50737f6a
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-01-20 22:07:31 +0100

    staging tech assert

--- definitions/declarations/httparchive.js
+++ definitions/declarations/httparchive.js
@@ -1,17 +1,39 @@
-const stagingTables = ['pages', 'requests', 'parsed_css']
-for (const table of stagingTables) {
+// Tables source: https://github.com/HTTPArchive/crawl/blob/main/crawl.py
+['pages', 'requests', 'parsed_css'].forEach(table =>
   declare({
-    schema: 'crawl_staging',
+    schema: 'wappalyzer',
     name: table
   })
-}
+)
 
-declare({
-  schema: 'wappalyzer',
-  name: 'technologies'
-})
+assert('corrupted_technology_values').query(ctx => `
+SELECT
+  date,
+  client,
+  tech,
+  COUNT(DISTINCT page) AS cnt_pages,
+  ARRAY_AGG(DISTINCT page LIMIT 3) AS sample_pages
+FROM ${ctx.ref('crawl_staging', 'pages')}
+LEFT JOIN pages.technologies AS tech
+LEFT JOIN tech.categories AS category
+WHERE
+  date = '${constants.currentMonth}' AND
+  (
+    tech.technology NOT IN (SELECT DISTINCT name FROM wappalyzer.technologies)
+    OR category NOT IN (SELECT DISTINCT name FROM wappalyzer.categories)
+    OR ARRAY_LENGTH(tech.categories) = 0
+  )
+GROUP BY
+  date,
+  client,
+  tech
+ORDER BY cnt DESC;
+`);
 
-declare({
-  schema: 'wappalyzer',
-  name: 'categories'
-})
+// Tables source: https://github.com/HTTPArchive/wappalyzer/blob/main/.github/workflows/upload.yml
+['technologies', 'categories'].forEach(table =>
+  declare({
+    schema: 'wappalyzer',
+    name: table
+  })
+)
