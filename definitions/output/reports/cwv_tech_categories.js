9ec8a813b94009c47a5568296886efb2bec8ace1
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-01-22 16:38:03 +0100

    Tech report: all categories origins (#51)
    * all categories
    
    * consistent naming
    
    * fix

--- definitions/output/reports/cwv_tech_categories.js
+++ definitions/output/reports/cwv_tech_categories.js
@@ -71,5 +71,22 @@ GROUP BY
   category,
   description,
   origins
-ORDER BY category ASC
+
+UNION ALL
+
+SELECT
+  'ALL' AS category,
+  NULL AS description,
+  STRUCT(
+    COALESCE(MAX(IF(client = 'desktop', origins, 0))) AS desktop,
+    COALESCE(MAX(IF(client = 'mobile', origins, 0))) AS mobile
+  ) AS origins,
+  NULL AS technologies
+FROM (
+  SELECT
+    client,
+    COUNT(DISTINCT root_page) AS origins
+  FROM pages
+  GROUP BY client
+)
 `)
