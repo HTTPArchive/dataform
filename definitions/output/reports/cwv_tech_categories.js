9d7610ca35ec429415e9127958dd8060a64a896f
Max Ostapenko <max@httparchive.org>
2025-01-22 22:29:03 +0000

    update tags

--- definitions/output/reports/cwv_tech_categories.js
+++ definitions/output/reports/cwv_tech_categories.js
@@ -3,7 +3,7 @@ const pastMonth = constants.fnPastMonth(constants.currentMonth)
 publish('cwv_tech_categories', {
   schema: 'reports',
   type: 'table',
-  tags: ['crux_ready']
+  tags: ['crux_ready', 'tech_report']
 }).query(ctx => `
 /* {"dataform_trigger": "report_cwv_tech_complete", "name": "categories", "type": "dict"} */
 WITH pages AS (
