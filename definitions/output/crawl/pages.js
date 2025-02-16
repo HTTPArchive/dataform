ba2a36dc499a0e6953d19ef849f160287e575953
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-02-16 19:44:43 +0100

    reservation assignments

--- definitions/output/crawl/pages.js
+++ definitions/output/crawl/pages.js
@@ -47,7 +47,8 @@ publish('pages', {
     technologies: 'Technologies detected at runtime (see https://www.wappalyzer.com/)',
     metadata: 'Additional metadata about the test'
   },
-  tags: ['crawl_complete']
+  tags: ['crawl_complete'],
+  dependencies: ['create_reservation_assignment']
 }).preOps(ctx => `
 DELETE FROM ${ctx.self()}
 WHERE date = '${constants.currentMonth}' AND
