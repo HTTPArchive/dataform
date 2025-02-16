739ffd3080fa052e376b6ec2d44a4df8dec98daf
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-02-16 15:27:20 +0100

    migrated from scheduled queries

---
+++ definitions/output/core_web_vitals/technology_matrix.js
@@ -0,0 +1,56 @@
+publish('technology_matrix', {
+  type: 'table',
+  schema: 'core_web_vitals',
+  bigquery: {
+    partitionBy: 'date',
+    clusterBy: ['client', 't1']
+  },
+  description: 'Used in: https://lookerstudio.google.com/u/2/reporting/0ad64c8f-644a-40f9-93e4-0ccd5f72b33d',
+  tags: ['crawl_complete'],
+  dependOnDependencyAssertions: true
+}).query(ctx => `
+WITH a AS (
+  SELECT
+    date,
+    client,
+    root_page,
+    technology AS t1,
+    ARRAY_TO_STRING(ARRAY(SELECT category FROM UNNEST(categories) AS category ORDER BY category), ', ') AS c1
+  FROM ${ctx.ref('crawl', 'pages')},
+    UNNEST(technologies)
+  WHERE
+    date = '${constants.currentMonth}' AND
+    is_root_page
+), b AS (
+  SELECT
+    client,
+    root_page,
+    technology AS t2,
+    ARRAY_TO_STRING(ARRAY(SELECT category FROM UNNEST(categories) AS category ORDER BY category), ', ') AS c2
+  FROM ${ctx.ref('crawl', 'pages')},
+    UNNEST(technologies)
+  WHERE
+    date = '${constants.currentMonth}' AND
+    is_root_page
+)
+
+
+SELECT
+  date,
+  client,
+  t1,
+  c1,
+  t2,
+  c2,
+  COUNT(DISTINCT root_page) AS pages
+FROM a
+LEFT JOIN b
+USING (client, root_page)
+GROUP BY
+  date,
+  client,
+  t1,
+  c1,
+  t2,
+  c2
+`)
