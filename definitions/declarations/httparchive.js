2470a8bedc34818f316c7406cdabff8f47dbf5c3
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-03-02 17:29:37 +0100

    reservation off

--- definitions/declarations/httparchive.js
+++ definitions/declarations/httparchive.js
@@ -42,7 +42,7 @@ ORDER BY cnt_pages DESC
 )
 
 operate('create_reservation_assignment')
-  .tags(['crawl_complete'])
+  //.tags(['crawl_complete'])
   .queries(ctx => `
 CREATE ASSIGNMENT
 \`httparchive.region-us.retrospective-reprocessing.pipeline\`
@@ -52,8 +52,8 @@ OPTIONS (
 `)
 
 operate('drop_reservation_assignment')
-  .dependencies(['requests_10k'])
-  .tags(['crawl_complete'])
+  //.dependencies(['requests_10k'])
+  //.tags(['crawl_complete'])
   .queries(ctx => `
 DROP ASSIGNMENT IF EXISTS
 \`httparchive.region-us.retrospective-reprocessing.pipeline\`
