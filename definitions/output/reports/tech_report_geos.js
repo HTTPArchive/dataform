f0d7fb02deb605ab1c6f75c8099674f5997a0727
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-04-28 00:58:58 +0200

    ranks and geos

---
+++ definitions/output/reports/tech_report_geos.js
@@ -0,0 +1,31 @@
+const pastMonth = constants.fnPastMonth(constants.currentMonth)
+
+publish('tech_report_geos', {
+  schema: 'reports',
+  type: 'table',
+  tags: ['tech_report']
+}).query(ctx => `
+SELECT
+  geo,
+  adoption AS origins
+FROM ${ctx.ref('reports', 'tech_report_adoption')}
+WHERE
+  date = '${pastMonth}'
+  AND rank = 'ALL'
+  AND technology = 'ALL'
+  AND version = 'ALL'
+  ${constants.devRankFilter}
+`).postOps(ctx => `
+  SELECT
+    reports.run_export_job(
+      JSON '''{
+        "destination": "firestore",
+        "config": {
+          "database": "tech-report-api-${constants.environment}",
+          "collection": "geos",
+          "type": "dict"
+        },
+        "query": "SELECT * FROM ${ctx.self()}"
+      }'''
+    );
+  `)
