d70441eef1f1c11021842d857f9a0bf1e19a75b1
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-05-12 00:05:28 +0200

    cleanup

---
+++ infra/tf/functions/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  type = string
+}
+
+variable "function_identity" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "edit_datasets" {
+  type = list(string)
+  default = []
+}
