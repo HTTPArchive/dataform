3935d6a4e728f894f287d9aa8a278e5789b03505
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-12-12 12:48:29 +0100

    Update Google provider versions to 7.13.0 and refresh hashes

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.9.7"
+  required_version = ">= 1.11.4"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.40.0"
+      version = "7.13.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 6.40.0"
+      version = "7.13.0"
     }
   }
 
