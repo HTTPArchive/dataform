affe445487722f73054bd8f69475afcc04d53156
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-12-14 20:57:19 +0100

    lint

--- infra/variables.tf
+++ infra/variables.tf
@@ -24,16 +24,16 @@ variable "location" {
 
 variable "function_identity" {
   default = "cloud-function@httparchive.iam.gserviceaccount.com"
-  type = string
+  type    = string
 }
 
 variable "dataform_service_account_email" {
   default = "service-226352634162@gcp-sa-dataform.iam.gserviceaccount.com"
-  type = string
+  type    = string
 }
 
 variable "edit_datasets" {
- default = [
+  default = [
     "crawl",
     "sample_data",
     "wappalyzer",
