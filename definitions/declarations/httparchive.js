be0575503267c56df6dc2f5e01d452c76b4cc307
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2026-07-19 22:32:33 +0200

    refactor: update Dataform definitions, simplify linting scripts, and clean up workflow configurations
    Signed-off-by: Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>

--- definitions/declarations/httparchive.js
+++ definitions/declarations/httparchive.js
@@ -14,7 +14,7 @@
   })
 )
 
-operations('httparchive_project_options').queries(`
+operate('httparchive_project_options').queries(`
 ALTER PROJECT httparchive SET OPTIONS (
   \`region-US.default_sql_dialect_option\` = 'only_google_sql',
   \`region-US.default_query_optimizer_options\` = 'adaptive=on',
@@ -22,4 +22,4 @@ ALTER PROJECT httparchive SET OPTIONS (
   \`region-US.enable_reservation_based_fairness\` = 'true',
   \`region-US.preflight_fluid_autoscaling_reservations\` = ['pipeline', 'interactive']
 );
-`);
+`)
