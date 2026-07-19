f49fd3bdfc5dddd9da8c81cdc6ab9facc75c4b12
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2026-07-19 22:18:53 +0200

    refactor: add project configuration to operations and move restore documentation to markdown
    Signed-off-by: Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>

--- definitions/declarations/httparchive.js
+++ definitions/declarations/httparchive.js
@@ -13,3 +13,13 @@
     name: table
   })
 )
+
+operations('httparchive_project_options').queries(`
+ALTER PROJECT httparchive SET OPTIONS (
+  \`region-US.default_sql_dialect_option\` = 'only_google_sql',
+  \`region-US.default_query_optimizer_options\` = 'adaptive=on',
+  \`region-US.query_runtime\` = 'advanced',
+  \`region-US.enable_reservation_based_fairness\` = 'true',
+  \`region-US.preflight_fluid_autoscaling_reservations\` = ['pipeline', 'interactive']
+);
+`);
